1530076eee33bd45d5a45a939520e80bd1341003
Markus Teich <markus.teich@stusta.mhn.de>
2013-10-08 01:53:55 +0200

    beautify regexes and blame command generation

--- git_cc_cmd.py
+++ git_cc_cmd.py
@@ -5,11 +5,11 @@ import subprocess
 import sys
 
 # compile regexes
-re_from = re.compile(r'From\s([a-f0-9]+)')
-re_hash = re.compile(r'---\sa/(\S+)')
-re_diff = re.compile(r'@@\s-([0-9]+),([0-9]+)')
-re_auth = re.compile(r'author\s([\w ]*\w+)')
-re_mail = re.compile(r'author-mail\s<(\S+@\S+\.\S+)>\n$')
+re_from = re.compile(r'From ([a-f0-9]+)')
+re_hash = re.compile(r'--- a/(\S+)')
+re_diff = re.compile(r'@@ -([0-9]+),([0-9]+)')
+re_auth = re.compile(r'author (.+)\n$')
+re_mail = re.compile(r'author-mail <(.+)>\n$')
 
 authors = {}
 
@@ -30,7 +30,7 @@ def parse_blame(f):
 for filename in sys.argv[1:]:
     with open(filename) as f:
         ma_source = None
-        cmd = ['git', 'blame', '--porcelain', '-L', '', '', None]
+        cmd = ['git', 'blame', '--porcelain', '-L', None, None, None]
         for line in f:
             if not cmd[-1]:
                 m = re_from.match(line)
@@ -45,7 +45,7 @@ for filename in sys.argv[1:]:
             elif cmd[-2]:
                 m = re_diff.match(line)
                 if m:
-                    cmd[-3] = ''.join([m.group(1), ',+', m.group(2)])
+                    cmd[-3] = m.group(1) + ',+' + m.group(2)
                     proc_blame = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                                                   universal_newlines=True)
                     parse_blame(proc_blame.stdout)
